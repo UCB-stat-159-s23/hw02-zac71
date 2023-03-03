ebfba2e356c0b6607b504cb64ff121a6e1fc816a
Zac Dong <zacdong@berkeley.edu>
2023-03-03 03:04:06 +0000

    updated imports

--- ligotools/__init__.py
+++ ligotools/__init__.py
@@ -1,1 +1,11 @@
-from ligotools import readligo as rl
+from .readligo import read_frame
+from .readligo import read_hdf5
+from .readligo import loaddata
+from .readligo import dq2segs
+from .readligo import dq_channel_to_seglist
+from .readligo import getsegs
+from .readligo import getstrain
+
+from .readligo import SegmentList
+from .readligo import FileList
+
